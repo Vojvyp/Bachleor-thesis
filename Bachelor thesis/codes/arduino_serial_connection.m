edfb8c723d7b131a4fc3a99f93db72b09625313f
Vojtěch Vyplel <vojvyp@gmail.com>
2021-03-30 20:41:37 +0200

    Update arduino_serial_connection.m

--- Bachelor thesis/codes/arduino_serial_connection.m
+++ Bachelor thesis/codes/arduino_serial_connection.m
@@ -1,8 +1,8 @@
 clear all
 clc
 COM = "COM3"
-
-x=1:800;                          % record length       
+% x = linspace(100, 900, 1000);
+x=1:1:300;                          % record length       
 serial = serialport(COM(1),9600); % open serial port for comunication with arduino
 
 
@@ -12,6 +12,17 @@ for i=1:length(x)                 % read data and string transfer to double form
 end    
     
 disp('making plot..')
-plot(x,y);
 
+y=y(20:length(x));
+% fs=187.86;
+fs=320;
+T=1/fs;
+L=length(y);
+time = (0:L-1)*T;
 
+[pks,locs] = findpeaks(y,time);
+period = max(diff(locs))
+
+freq =(1/period)
+%y=y*0.0048876;
+plot(time,y);
